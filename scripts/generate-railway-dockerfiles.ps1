df1aa9dabc6984ad0d13925ed5aa149cab340b5a
Emre Erbek <emreerbek@geoproje.com.tr>
2026-06-27 21:37:00 +0300

    Railway Dockerfile: bind mount ve cache mount kaldir, COPY kullan

--- scripts/generate-railway-dockerfiles.ps1
+++ scripts/generate-railway-dockerfiles.ps1
@@ -1,9 +1,81 @@
-# Baserow Dockerfile'larindan Railway uyumlu surumler uretir (sadece cache mount kaldirilir).
+# Baserow Dockerfile'larindan Railway uyumlu surumler uretir.
+# Railway Metal builder BuildKit mount'lari desteklemez (cache + bind).
 $ErrorActionPreference = "Stop"
 $RepoRoot = Split-Path $PSScriptRoot -Parent
 
+function Get-UnixParentPath {
+    param([string]$Path)
+    $idx = $Path.LastIndexOf('/')
+    if ($idx -le 0) { return $Path }
+    return $Path.Substring(0, $idx)
+}
+
+function Get-RunBlockEndIndex {
+    param([string[]]$Lines, [int]$StartIndex)
+    $end = $StartIndex
+    while ($end -lt $Lines.Count -and $Lines[$end] -match '\\\s*$') {
+        $end++
+    }
+    return $end
+}
+
+function Convert-RunBlockForRailway {
+    param([string[]]$RunLines)
+
+    $fullBlock = $RunLines -join "`n"
+    $bindPattern = '--mount=type=bind,source=([^,]+),target=([^\s\\]+)'
+    $bindMatches = [regex]::Matches($fullBlock, $bindPattern)
+
+    $copyLines = New-Object System.Collections.Generic.List[string]
+    if ($bindMatches.Count -gt 0) {
+        $byDestDir = [ordered]@{}
+        foreach ($m in $bindMatches) {
+            $source = $m.Groups[1].Value
+            $target = $m.Groups[2].Value
+            $destDir = Get-UnixParentPath -Path $target
+            if (-not $byDestDir.Contains($destDir)) {
+                $byDestDir[$destDir] = [System.Collections.Generic.List[string]]::new()
+            }
+            if (-not $byDestDir[$destDir].Contains($source)) {
+                [void]$byDestDir[$destDir].Add($source)
+            }
+        }
+        foreach ($destDir in $byDestDir.Keys) {
+            $sources = ($byDestDir[$destDir] -join ' ')
+            $copyLines.Add("COPY --chown=`$UID:`$GID $sources $destDir/")
+        }
+    }
+
+    $cmdLines = New-Object System.Collections.Generic.List[string]
+    foreach ($line in $RunLines) {
+        if ($line -match '^\s*--mount=') { continue }
+
+        $stripped = [regex]::Replace(
+            $line,
+            '(^RUN\s+)(?:--mount=type=(?:cache|bind)[^\s\\]+\s*)+',
+            'RUN '
+        )
+        $stripped = $stripped.TrimEnd()
+        if ($stripped -match '^RUN\s*\\?\s*$') { continue }
+        if ($stripped) {
+            [void]$cmdLines.Add($stripped)
+        }
+    }
+
+    if ($cmdLines.Count -eq 0) {
+        throw "RUN blogu komut satiri icermiyor: $fullBlock"
+    }
+
+    if ($cmdLines[0] -notmatch '^RUN\s') {
+        $cmdLines[0] = 'RUN ' + $cmdLines[0].TrimStart()
+    }
+
+    return $copyLines, ($cmdLines -join "`n")
+}
+
 function Convert-RailwayDockerfile {
     param([string]$Source, [string]$Destination)
+
     $result = New-Object System.Collections.Generic.List[string]
     $lines = Get-Content $Source
 
@@ -14,20 +86,15 @@ function Convert-RailwayDockerfile {
             continue
         }
 
-        if ($line -match '^RUN --mount=type=cache') {
-            $next = $i + 1
-            while ($next -lt $lines.Count -and $lines[$next] -match '^\s+--mount=type=cache') {
-                $next++
+        if ($line -match '^RUN\s+--mount=') {
+            $end = Get-RunBlockEndIndex -Lines $lines -StartIndex $i
+            $runLines = $lines[$i..$end]
+            $copyLines, $cleanRun = Convert-RunBlockForRailway -RunLines $runLines
+            foreach ($copyLine in $copyLines) {
+                $result.Add($copyLine)
             }
-            if ($next -lt $lines.Count -and $lines[$next] -match '^\s+--mount=type=bind') {
-                $result.Add('RUN ' + $lines[$next].TrimStart())
-                $i = $next
-            } else {
-                if ($next -lt $lines.Count) {
-                    $result.Add('RUN ' + $lines[$next].TrimStart())
-                    $i = $next
-                }
-            }
+            $result.Add($cleanRun)
+            $i = $end
             continue
         }
 
